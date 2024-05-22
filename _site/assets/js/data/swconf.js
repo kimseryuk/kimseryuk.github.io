40804bafef6de7700cd8c8cf35af15be13bc913e
kimseryuk <kimseryuk@gmail.com>
2024-05-22 11:22:03 +0900

    24-05-22 published

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716281276',resources: [
+    cacheName: 'chirpy-1716344463',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
